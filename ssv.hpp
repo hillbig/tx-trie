19901bb00ce60059e0e8b370b468052a35d784f2
Daisuke.Okanohara <Daisuke.Okanohara@76b497fe-a64c-0410-acda-eb81fd70df80>
2010-05-17 05:43:09 +0000

    add license

--- ssv.hpp
+++ ssv.hpp
@@ -1,3 +1,22 @@
+/* 
+ *  Copyright (c) 2007-2010 Daisuke Okanohara
+ * 
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ * 
+ *   1. Redistributions of source code must retain the above Copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *
+ *   2. Redistributions in binary form must reproduce the above Copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *
+ *   3. Neither the name of the authors nor the names of its contributors
+ *      may be used to endorse or promote products derived from this
+ *      software without specific prior written permission.
+ */
+
 #ifndef __SSV_HPP__
 #define __SSV_HPP__
 
